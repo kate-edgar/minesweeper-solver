83774c718c666f3b23535d8472c11b40c1a3200f
kate-edgar <116528682+kate-edgar@users.noreply.github.com>
2023-07-09 18:38:55 -0400

    Update Logic.py

--- Logic.py
+++ Logic.py
@@ -1,18 +1,18 @@
-# Solves a given board 
+# Solves a given board
 
 import Board
 from Board import board
 
 
-def check_bomb(board, rows, columns):
+def check_mine(board, rows, columns):
     available_spaces = []
     for i in range(rows):
         for j in range(columns):
             if board[i][j] != 0 and board[i][j] != 'y':
                 available_spaces.append(board[i][j])
-                potential_bomb = search_area(i, j, rows, columns, 'y')
-                if len(potential_bomb) == board[i][j]:
-                    board[potential_bomb[0][0]][potential_bomb[0][1]] = 'X'
+                potential_mine = search_area(i, j, rows, columns, 'y')
+                if len(potential_mine) == board[i][j]:
+                    board[potential_mine[0][0]][potential_mine[0][1]] = 'X'
 
 
 def check_safe(board, rows, columns):
@@ -67,77 +67,77 @@ def bottom_right(x, y, search):
 
 
 def search_area(x, y, num_rows, num_col, search):
-    potential_bombs = []
+    potential_mines = []
 
     # Case 1: Top left corner
     if y == 0 and x == 0:
-        potential_bombs.append(middle_right(x, y, search))
-        potential_bombs.append(bottom_right(x, y, search))
-        potential_bombs.append(bottom_middle(x, y, search))
+        potential_mines.append(middle_right(x, y, search))
+        potential_mines.append(bottom_right(x, y, search))
+        potential_mines.append(bottom_middle(x, y, search))
 
     # Case 2: Top right corner
     elif y == 0 and x == num_rows-1:
-        potential_bombs.append(middle_left(x, y, search))
-        potential_bombs.append(bottom_left(x, y, search))
-        potential_bombs.append(bottom_middle(x, y, search))
+        potential_mines.append(middle_left(x, y, search))
+        potential_mines.append(bottom_left(x, y, search))
+        potential_mines.append(bottom_middle(x, y, search))
 
     # Case 3: Bottom left corner
     elif y == num_col - 1 and x == 0:
-        potential_bombs.append(top_middle(x, y, search))
-        potential_bombs.append(top_right(x, y, search))
-        potential_bombs.append(middle_right(x, y, search))
+        potential_mines.append(top_middle(x, y, search))
+        potential_mines.append(top_right(x, y, search))
+        potential_mines.append(middle_right(x, y, search))
 
     # Case 4: Bottom right corner
     elif y == num_col-1 and x == num_rows-1:
-        potential_bombs.append(top_left(x, y, search))
-        potential_bombs.append(top_middle(x, y, search))
-        potential_bombs.append(middle_left(x, y, search))
+        potential_mines.append(top_left(x, y, search))
+        potential_mines.append(top_middle(x, y, search))
+        potential_mines.append(middle_left(x, y, search))
 
     # Case 5: Against Left
     elif x == 0:
-        potential_bombs.append(top_middle(x, y, search))
-        potential_bombs.append(top_right(x, y, search))
-        potential_bombs.append(middle_right(x, y, search))
-        potential_bombs.append(bottom_right(x, y, search))
-        potential_bombs.append(bottom_middle(x, y, search))
+        potential_mines.append(top_middle(x, y, search))
+        potential_mines.append(top_right(x, y, search))
+        potential_mines.append(middle_right(x, y, search))
+        potential_mines.append(bottom_right(x, y, search))
+        potential_mines.append(bottom_middle(x, y, search))
 
     # Case 2: Against Top
     elif y == 0:
-        potential_bombs.append(middle_left(x, y, search))
-        potential_bombs.append(middle_right(x, y, search))
-        potential_bombs.append(bottom_left(x, y, search))
-        potential_bombs.append(bottom_middle(x, y, search))
-        potential_bombs.append(bottom_right(x, y, search))
+        potential_mines.append(middle_left(x, y, search))
+        potential_mines.append(middle_right(x, y, search))
+        potential_mines.append(bottom_left(x, y, search))
+        potential_mines.append(bottom_middle(x, y, search))
+        potential_mines.append(bottom_right(x, y, search))
 
     # Case 3: Against Bottom
     elif y == num_col-1:
-        potential_bombs.append(top_left(x, y, search))
-        potential_bombs.append(top_middle(x, y, search))
-        potential_bombs.append(top_right(x, y, search))
-        potential_bombs.append(middle_left(x, y, search))
-        potential_bombs.append(middle_right(x, y, search))
+        potential_mines.append(top_left(x, y, search))
+        potential_mines.append(top_middle(x, y, search))
+        potential_mines.append(top_right(x, y, search))
+        potential_mines.append(middle_left(x, y, search))
+        potential_mines.append(middle_right(x, y, search))
 
     # Case 4: Against Right
     elif x == num_rows-1:
-        potential_bombs.append(top_left(x, y, search))
-        potential_bombs.append(top_middle(x, y, search))
-        potential_bombs.append(middle_left(x, y, search))
-        potential_bombs.append(bottom_left(x, y, search))
-        potential_bombs.append(bottom_middle(x, y, search))
+        potential_mines.append(top_left(x, y, search))
+        potential_mines.append(top_middle(x, y, search))
+        potential_mines.append(middle_left(x, y, search))
+        potential_mines.append(bottom_left(x, y, search))
+        potential_mines.append(bottom_middle(x, y, search))
 
     # Case 9: Middle
     else:
-        potential_bombs.append(top_left(x, y, search))
-        potential_bombs.append(top_middle(x, y, search))
-        potential_bombs.append(top_right(x, y, search))
-        potential_bombs.append(middle_left(x, y, search))
-        potential_bombs.append(middle_right(x, y, search))
-        potential_bombs.append(bottom_left(x, y, search))
-        potential_bombs.append(bottom_middle(x, y, search))
-        potential_bombs.append(bottom_right(x, y, search))
+        potential_mines.append(top_left(x, y, search))
+        potential_mines.append(top_middle(x, y, search))
+        potential_mines.append(top_right(x, y, search))
+        potential_mines.append(middle_left(x, y, search))
+        potential_mines.append(middle_right(x, y, search))
+        potential_mines.append(bottom_left(x, y, search))
+        potential_mines.append(bottom_middle(x, y, search))
+        potential_mines.append(bottom_right(x, y, search))
 
-    new_potential_bombs = list(filter(None, potential_bombs))
-    return new_potential_bombs
+    new_potential_mines = list(filter(None, potential_mines))
+    return new_potential_mines
 
 
 columns = 10
@@ -149,7 +149,7 @@ board = board()
 print("Starting board:")
 Board.print_board(board, rows)
 
-check_bomb(board, rows, columns)
+check_mine(board, rows, columns)
 check_safe(board, rows, columns)
 
 print('Solution:')
